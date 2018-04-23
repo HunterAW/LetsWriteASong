7b562fdbd58c8f884ff182d981ab47b9be6d0d0a
Hunter Wright <wright.hunter@gmail.com>
2018-04-23 15:07:07 -0500

    Creating the foundation.

---
+++ LetsWriteASong/LetsWriteASong/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LetsWriteASong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select models to add to print");
+            Console.WriteLine("Dragon, Duck, Robot.");
+            Console.ReadLine();
+        }
+    }
+}
